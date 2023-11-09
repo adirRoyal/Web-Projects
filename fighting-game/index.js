63a9726e57145a7eeb6e5ad4399e6c6bff299038
adirRoyal <119738846+adirRoyal@users.noreply.github.com>
2023-11-10 00:22:16 +0200

    Update index.js
    fix run idel

--- fighting-game/index.js
+++ fighting-game/index.js
@@ -38,7 +38,24 @@ const player = new Fighter({
     offset: {
         x: 0,
         y: 0
-    }
+    },
+    imageSrc: './img/samuraiMack/Idle.png',
+    frameMax: 8,
+    scale: 2.5,
+    offset: {
+        x: 215,
+        y: 157
+    },
+    sprites: {
+        idle: {
+            imageSrc: './img/samuraiMack/Idle.png',
+            frameMax: 8
+        },
+        run: {
+            imageSrc: './img/samuraiMack/Run.png',
+            frameMax: 8
+        }
+    },
 })
 
 const enemy = new Fighter({
@@ -84,16 +101,20 @@ function animate() {
     background.update();
     shop.update();
     player.update();
-    enemy.update();
-
+    //enemy.update();
+    
     player.velocity.x = 0;
     enemy.velocity.x = 0;
 
     //player movment
+    player.image = player.sprites.idle.image
     if (keys.a.pressed && player.lastKey === 'a') {
         player.velocity.x = -5
+        player.image = player.sprites.run.image
+
     } else if (keys.d.pressed && player.lastKey === 'd') {
         player.velocity.x = 5
+        player.image = player.sprites.run.image
     }
 
     //enemy movment
