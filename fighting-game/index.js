9d27cd5ee94973d93e1161a847f3b89d6a384363
adirRoyal <119738846+adirRoyal@users.noreply.github.com>
2023-11-09 19:23:02 +0200

    update work don't finish

--- fighting-game/index.js
+++ fighting-game/index.js
@@ -7,63 +7,26 @@ canvas.height = 576;
 c.fillRect(0, 0, canvas.width, canvas.height)
 
 const gravity = 0.7;
-class Sprite {
-    constructor({ position, velocity, color = 'red', offset }) {
-        this.position = position;
-        this.velocity = velocity;
-        this.width = 50;
-        this.height = 150;
-        this.lastKey;
-        this.attackBox = {
-            position: {
-                x: this.position.x,
-                y: this.position.y
-            },
-            offset,
-            width: 100,
-            height: 50
-        }
-        this.color = color;
-        this.isAttacking
-    }
 
-    draw() {
-        c.fillStyle = this.color;
-        c.fillRect(this.position.x, this.position.y, this.width, this.height);
+const background = new Sprite({
+    position: {
+        x: 0,
+        y: 0
+    },
+    imageSrc: './img/background.png'
+})
 
-        //attack box 
-        if (this.isAttacking) {
-            c.fillStyle = 'green';
-            c.fillRect(
-                this.attackBox.position.x,
-                this.attackBox.position.y,
-                this.attackBox.width,
-                this.attackBox.height
-            )
-        }
-    }
+const shop = new Sprite({
+    position: {
+        x: 600,
+        y: 128
+    },
+    imageSrc: './img/shop.png',
+    scale: 2.75,
+    frameMax: 6
+})
 
-    update() {
-        this.draw();
-        this.attackBox.position.x = this.position.x + this.attackBox.offset.x;
-        this.attackBox.position.y = this.position.y;
-
-        this.position.x += this.velocity.x;
-        this.position.y += this.velocity.y;
-
-        if (this.position.y + this.height + this.velocity.y >= canvas.height) {
-            this.velocity.y = 0
-        } else this.velocity.y += gravity;
-    }
-    attack() {
-        this.isAttacking = true;
-        setTimeout(() => {
-            this.isAttacking = false;
-        },100)
-    }
-}
-
-const player = new Sprite({
+const player = new Fighter({
     position: {
         x: 0,
         y: 0
@@ -74,11 +37,11 @@ const player = new Sprite({
     },
     offset: {
         x: 0,
-        y:0
+        y: 0
     }
 })
 
-const enemy = new Sprite({
+const enemy = new Fighter({
     position: {
         x: 400,
         y: 100
@@ -93,8 +56,6 @@ const enemy = new Sprite({
         y: 0
     }
 })
-
-
 
 console.log(player);
 
@@ -113,19 +74,15 @@ let keys = {
     }
 }
 
-function rectangularCollision({ rectangle1, rectangle2 }) {
-    return (
-        rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x &&
-        rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width &&
-        rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y &&
-        rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
-    );
-}
+
+decreaseTimer();
 
 function animate() {
     window.requestAnimationFrame(animate);
     c.fillStyle = 'black';
     c.fillRect(0, 0, canvas.width, canvas.height);
+    background.update();
+    shop.update();
     player.update();
     enemy.update();
 
@@ -155,7 +112,8 @@ function animate() {
         player.isAttacking
     ) {
         player.isAttacking = false;
-        console.log('player attack successful');
+        enemy.health -= 20;
+        document.querySelector('#enemyHealth').style.width = enemy.health + '%'
     }
 
     if (
@@ -166,8 +124,15 @@ function animate() {
         enemy.isAttacking
     ) {
         enemy.isAttacking = false;
-        console.log('enemy attack successful');
+        player.health -= 20;
+        document.querySelector('#playerHealth').style.width = player.health + '%'
     }
+
+    // end game basef on health
+    if (enemy.health <= 0 || player.health <= 0) {
+        determineWinner({ player, enemy, timerId });
+    }
+
 }
 animate();
 
@@ -176,11 +141,11 @@ window.addEventListener('keydown', (event) => {
     switch (event.key) {
         case 'd':
             keys.d.pressed = true;
-            player.lastKey = 'd';
+            player.lastKey = 'd'
             break;
         case 'a':
             keys.a.pressed = true;
-            player.lastKey = 'a';
+            player.lastKey = 'a'
             break;
         case 'w':
             player.velocity.y = -20;
